ace7e23693bf1aa2d54391e7ebb9f4f8f4ce91f3
Sangguk Lee <sangguk258@gmail.com>
2023-08-23 20:55:16 +0900

    persist config

--- src/store/PineconeConfigStore.ts
+++ src/store/PineconeConfigStore.ts
@@ -1,15 +1,24 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface PineconeConfig {
-  environment: string,
-  apiKey: string,
-  setEnvironment: (environment: string) => void,
-  setApiKey: (apiKey: string) => void,
+  environment: string;
+  apiKey: string;
+  setEnvironment: (environment: string) => void;
+  setApiKey: (apiKey: string) => void;
 }
 
-export const usePineconeConfigStore = create<PineconeConfig>((set) => ({
-  environment: '',
-  apiKey: '',
-  setEnvironment: (environment: string) => set({ environment }),
-  setApiKey: (apiKey: string) => set({ apiKey }),
-}))
+export const usePineconeConfigStore = create(
+  persist<PineconeConfig>(
+    (set) => ({
+      environment: "",
+      apiKey: "",
+      setEnvironment: (environment: string) => set({ environment }),
+      setApiKey: (apiKey: string) => set({ apiKey }),
+    }),
+    {
+      name: "pinecone-config",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
